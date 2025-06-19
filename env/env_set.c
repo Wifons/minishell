676601552074ba41ffd620f1d814d6c4ca8e149a
Wifons <williamfons6@gmail.com>
2025-06-19 22:11:44 +0200

    update

--- env/env_set.c
+++ env/env_set.c
@@ -1,85 +1,80 @@
 #include "../minishell.h"
 
-int dup_value(t_env_var *var, const char  *value)
+int dup_value(t_env_var *var, const char *value)
 {
-    var->value = ft_strdup(value);
-    if (var->value)
-        return (1);
-    return (0);
+	var->value = ft_strdup(value);
+	if (var->value)
+		return (1);
+	return (0);
 }
 
-static  t_env_var *create_env_var(const char *name, const char *value)
+static t_env_var *create_env_var(const char *name, const char *value)
 {
-    t_env_var *var;
-    
-    if (!name)
-        return (NULL);
-    var = ft_calloc(1, sizeof(t_env_var));
-    if (!var)
-        return (NULL);
-    var->name = ft_strdup(name);
-    if (!var->name)
-    {
-        free(var);
-        return (NULL);
-    }
-    if (value)
-    {
-        if (!dup_value(var, value))
-        {
-            free(var->name);
-            free(var);
-            return (NULL);
-        }
-    }
-    else
-        var->value = NULL;  // Set to NULL for no value, not empty string
-    return (var);
+	t_env_var *var;
+
+	if (!name)
+		return (NULL);
+	var = ft_calloc(1, sizeof(t_env_var));
+	if (!var)
+		return (NULL);
+	var->name = ft_strdup(name);
+	if (!var->name)
+	{
+		free(var);
+		return (NULL);
+	}
+	if (value)
+	{
+		if (!dup_value(var, value))
+		{
+			free(var->name);
+			free(var);
+			return (NULL);
+		}
+	}
+	else
+		var->value = NULL;
+	return (var);
 }
 
-static int  update_existing_var(t_env_var *var, const char *value)
+static int update_existing_var(t_env_var *var, const char *value)
 {
-    char  *new_val;
-    
-    if (!var)
-        return (-1);
-    
-    // If value is NULL (no = sign), only update if current value is also NULL
-    // This implements bash behavior where "export test" doesn't override existing values
-    if (!value)
-    {
-        if (var->value == NULL)
-            return (0);  // Already NULL, nothing to do
-        else
-            return (0);  // Has a value, don't change it
-    }
-    
-    // Update to new value (including empty string)
-    free(var->value);
-    var->value = NULL;
-    new_val = ft_strdup(value);
-    if (!new_val)
-        return (-1);
-    var->value = new_val;
-    return (0);
+	char *new_val;
+
+	if (!var)
+		return (-1);
+	if (!value)
+	{
+		if (var->value == NULL)
+			return (0);
+		else
+			return (0);
+	}
+	free(var->value);
+	var->value = NULL;
+	new_val = ft_strdup(value);
+	if (!new_val)
+		return (-1);
+	var->value = new_val;
+	return (0);
 }
 
 int env_set(t_env_var *env, const char *name, const char *val)
 {
-    t_env_var   *existing;
-    t_env_var   *new_node;
-    
-    if (!env || !name)
-        return (-1);
-    existing = ft_lstfind(env, (void *)name, env_var_cmp_name);
-    if (existing)
-        return (update_existing_var((t_env_var *)existing, val));
-    new_node = create_env_var(name, val);
-    if (!new_node)
-    {
-        ft_lstclear_env(env);
-        return (-1);
-    }
-    ft_lstadd_back_env(&env, new_node);
-    return (0);
+	t_env_var *existing;
+	t_env_var *new_node;
+
+	if (!env || !name)
+		return (-1);
+	existing = ft_lstfind(env, (void *)name, env_var_cmp_name);
+	if (existing)
+		return (update_existing_var((t_env_var *)existing, val));
+	new_node = create_env_var(name, val);
+	if (!new_node)
+	{
+		ft_lstclear_env(env);
+		return (-1);
+	}
+	ft_lstadd_back_env(&env, new_node);
+	return (0);
 }
